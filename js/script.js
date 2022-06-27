76706fd020ba2c6f95e42a8122b55c8b954059ce
Alexandra <alexandra.asv@gmail.com>
2022-06-27 19:41:38 +0200

    19. Практика ч1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
+"use strict";
 
-const category = 'toys';
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-console.log(`http://someurl/${category}`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const user = "Alex";
+let a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько вы бы его оценили?"), "",
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("На сколько вы бы его оценили?", "");
 
-alert(`Hello, ${user}`);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
